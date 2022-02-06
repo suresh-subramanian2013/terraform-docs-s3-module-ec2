005baf4cad0b611ef1cef4ff14b629df969a6ed5
suresh kumar <suressubramanian2013@gmail.com>
2022-02-06 20:53:09 +0530

    provideralias

--- provider.tf
+++ provider.tf
@@ -1,7 +1,14 @@
 provider "aws" {
   version = "~> 2.20.0"
+
   region = "us-east-1"
 }
+
+provider "terraform-docs" {
+  alias = "demo"
+}
+
+
 terraform {
   required_providers {
     consul = ">= 2.4.0"
